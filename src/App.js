198793837eab4f524dba995a116e698ece82643a
Aadarsh Nagrath <anagrath1@gmail.com>
2023-11-29 01:34:05 +0530

    setup & routes config

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import Home from "./pages/home/Home";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import Login from "./pages/login/Login";
+import Single from "./pages/single/Single";
+import List from "./pages/list/List";
+import New from "./pages/new/New";  
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/users">
+              <Route index element={<List />} />
+              <Route path=":userId" element={<Single />} />
+              <Route path="new" element={<New />} />
+            </Route>
+            <Route path="/products">
+              <Route index element={<List />} />
+              <Route path=":pId" element={<Single />} />
+              <Route path="new" element={<New />} />
+            </Route>
+          </Route>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
